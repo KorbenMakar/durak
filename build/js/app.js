b306e6f572ff60e8d965f260afe6e9b43414b092
Oleg Bronzov <oleg.bronzov@symmetrics.de>
2015-06-03 17:53:52 +0300

    JQuery UI has been added.

--- build/js/app.js
+++ build/js/app.js
@@ -1,5 +1,6 @@
 var App = function() {
     this._view = new View();
+    this._gameId = null;
 };
 
 App.prototype = {
@@ -22,6 +23,27 @@ App.prototype = {
      * @return null
      */
     startGame: function(playersCount) {
+        this._gameId = this.getGameId();
+        //TODO: init view cards.
         this._view.hideMenu();
+    },
+
+    /**
+     * Calls to the backend to get backend's game id.
+     *
+     * @return string
+     */
+    getGameId: function() {
+        //TODO: get game ID from backend.
+
+    },
+
+    /**
+     * Returns trump cards ID.
+     *
+     * @return string
+     */
+    getTrumpCardId: function() {
+        //TODO: implement.
     }
 };
