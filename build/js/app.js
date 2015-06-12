a195ec2930ffa979ac0e3a8386734968336e45fd
Oleg Bronzov <oleg.bronzov@symmetrics.de>
2015-06-12 11:02:12 +0300

    Moving cards has been implemented

--- build/js/app.js
+++ build/js/app.js
@@ -1,17 +1,16 @@
 var App = function() {
     this._view = new View();
     this._gameId = null;
+    this._trump = null;
 };
 
 App.prototype = {
     /**
      * Stops game.
      *
-     * @return null
+     * @return undefined
      */
     stopGame: function() {
-        //TODO: move all cards to the deck.
-
         this._view.showMenu();
     },
 
@@ -20,30 +19,68 @@ App.prototype = {
      *
      * @param int playersCount Count of players.
      *
-     * @return null
+     * @return undefined
      */
     startGame: function(playersCount) {
-        this._gameId = this.getGameId();
-        //TODO: init view cards.
+        var gameInfo = this.getGameInfo();
+        this._gameId = gameInfo.gameId;
+        this._trump = gameInfo.trump;
+        this._view.init(this._trump);
+
+        //TODO: init cards distribution
+        var cardNum = 0;
+        while (Object.keys(gameInfo.distribute).length) {
+            for (var playerId in gameInfo.distribute) {
+                var moveCardNum = cardNum;
+                gameInfo.distribute[playerId].count--;
+                cardNum++;
+
+                var cardClass;
+                if (gameInfo.distribute[playerId].count < 0) {
+                    delete gameInfo.distribute[playerId];
+                    continue;
+                } else if ('info' in gameInfo.distribute[playerId]) {
+                    cardClass = 'card' + gameInfo.distribute[playerId].info[(gameInfo.distribute[playerId].count - 1)];
+                }
+
+                this._view.moveCard(moveCardNum, playerId);
+            }
+        }
+
         this._view.hideMenu();
     },
 
     /**
-     * Calls to the backend to get backend's game id.
+     * Returns backend's game info.
      *
-     * @return string
+     * @return {}
      */
-    getGameId: function() {
-        //TODO: get game ID from backend.
-
-    },
-
-    /**
-     * Returns trump cards ID.
-     *
-     * @return string
-     */
-    getTrumpCardId: function() {
+    getGameInfo: function() {
         //TODO: implement.
+        return {
+            gameId: 'wd9skjc87389sdfks3ei0siokdj90',
+            trump: '9H',
+            distribute: {
+                player1: {
+                    count: 6
+                },
+                player2: {
+                    count: 6
+                },
+                player3: {
+                    count: 6
+                },
+                player4: {
+                    count: 6
+                },
+                player5: {
+                    count: 6,
+                    info: ['6D', 'AS', 'KC', 'KH', '7S', '9H']
+                },
+                player6: {
+                    count: 6
+                }
+            }
+        }
     }
 };
--- build/js/view.js
+++ build/js/view.js
@@ -1,34 +1,153 @@
 var View = function() {
-    this._menu = null;
+    this._containers = {};
+    this._cards = [];
 };
 
+View.CONTAINER_MENU = 'menu';
+View.CONTAINER_DECK = 'deck';
+View.CONTAINER_TRASH = 'trash';
+View.CONTAINER_TABLE = 'table';
+View.CONTAINER_PLAYER1 = 'player1';
+View.CONTAINER_PLAYER2 = 'player2';
+View.CONTAINER_PLAYER3 = 'player3';
+View.CONTAINER_PLAYER4 = 'player4';
+View.CONTAINER_PLAYER5 = 'player5';
+View.CONTAINER_PLAYER6 = 'player6';
+
 View.prototype = {
+    /**
+     * Initializes new game.
+     * Moves all cards into the deck.
+     * Sets up trump.
+     *
+     * @param string trump Card trump.
+     *
+     * @return undefined
+     */
+    init: function(trump) {
+        this.buildDeck(trump);
+    },
+
+    /**
+     * Moves card into the target.
+     *
+     * @param integer cardNum          Card number.
+     * @param string  target           Target id.
+     * @param string  addCompleteClass Add class on complete.
+     * @param string  onComplete       On complete callback.
+     *
+     * @return undefined
+     */
+    moveCard: function(cardNum, target, addCompleteClass, onComplete) {
+        var targetOffset = this.getContainer(target).offset();
+        var self = this;
+        this._cards[cardNum].animate({
+            left: (
+                targetOffset.left
+                    + ((this.getContainer(target).width() / 2) - (this._cards[cardNum].width() / 2))
+                ),
+            top: (
+                targetOffset.top
+                    + 2
+//                    + this.getContainer(target).height() / 2
+                )
+        }, {
+            complete: function() {
+                self._cards[cardNum].removeAttr('style').removeAttr('class').addClass('card card-back');
+
+                if (addCompleteClass) {
+                    self._cards[cardNum].addClass(addCompleteClass);
+                }
+
+                self._cards[cardNum].css({position: 'static'});
+                self.getContainer(target).append(self._cards[cardNum]);
+
+                if (onComplete) {
+                    onComplete();
+                }
+            }
+        });
+    },
+
+    /**
+     * Builds deck.
+     *
+     * @param string Card trump.
+     *
+     * @return undefined
+     */
+    buildDeck: function(trump) {
+        $('.card').remove();
+        this._cards = [];
+        var rotateList = [
+            'card-rotate-left',
+            'card-rotate-right',
+            ''
+        ];
+
+        for (var iter = 0; iter < 36; iter++) {
+            var $card = $('<div class="card"></div>');
+
+            if (iter == 35) {
+                $card.addClass('trump-card');
+                $card.addClass('card' + trump);
+            } else {
+                $card.addClass('card-back');
+                $card.addClass(rotateList[Math.floor(Math.random()*rotateList.length)]);
+            }
+
+            this.getDeck().append($card);
+
+            this._cards.push($card);
+        }
+    },
+
+    /**
+     * Returns container object.
+     *
+     * @param string containerName Container name.
+     *
+     * @return $
+     */
+    getContainer: function(containerName) {
+        if (!(containerName in this._containers)) {
+            this._containers[containerName] = $('#' + containerName);
+        }
+
+        return this._containers[containerName];
+    },
+
     /**
      * Returns menu object.
      *
      * @return $
      */
     getMenu: function() {
-        if (!this._menu) {
-            this._menu = $('#menu');
-        }
+        return this.getContainer(View.CONTAINER_MENU);
+    },
 
-        return this._menu;
+    /**
+     * Returns deck object.
+     *
+     * @return $
+     */
+    getDeck: function() {
+        return this.getContainer(View.CONTAINER_DECK);
     },
 
     /**
      * Shows menu.
      *
-     * @return null
+     * @return undefined
      */
     showMenu: function() {
-        this.getMenu().css({opacity: 1.0}).show();
+        this.getMenu().show();
     },
 
     /**
      * Hides menu.
      *
-     * @return null
+     * @return undefined
      */
     hideMenu: function() {
         this.getMenu().hide();
